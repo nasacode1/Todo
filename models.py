1b9cb744200b6bb78e52b116171dab1ca3536a27
Maanasa Ganesh <maanasarare@gmail.com>
2024-11-04 22:55:01 +0530

    Inheriting the base class and setting the attributes

--- models.py
+++ models.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from .database import Base
 from datetime import datetime
 
+class ToDo(Base):
+    __tablename__= "todos"
+
+     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    date_created = Column(DateTime, default=datetime.utcnow)
+    date_to_be_completed = Column(DateTime, nullable=True)
+    is_important = Column(Boolean, default=False)
